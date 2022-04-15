3c964c5790b11e1f333976cfcb53070f8623a0f9
leo-albergaria <leo.albergaria@icloud.com>
2022-04-15 17:30:41 +0200

    atual 150422

---
+++ Temporario/Avaliadas/C00_Antonio/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antthoma <antthoma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/07 03:50:07 by antthoma          #+#    #+#             */
+/*   Updated: 2022/04/07 12:57:07 by antthoma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	int	i;
+
+	i = 122;
+	while (i >= 97)
+	{
+		ft_putchar(i);
+		i--;
+	}
+}
