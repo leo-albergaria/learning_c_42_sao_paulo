d81586a43080da0667b3ee1e79f7ca17ee7ce80f
leo-albergaria <leo.albergaria@icloud.com>
2022-04-22 14:07:30 +0200

    C03_v02

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -6,20 +6,20 @@
 /*   By: lealberg <lealberg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 13:05:10 by lealberg          #+#    #+#             */
-/*   Updated: 2022/04/15 13:08:23 by lealberg         ###   ########.fr       */
+/*   Updated: 2022/04/22 13:25:51 by lealberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	int	i;
-	int	j;
+	int				i;
+	unsigned int	j;
 
 	i = 0;
 	while (dest[i])
 		i++;
 	j = 0;
-	while (src[j] && nb > j)
+	while (src[j] != '\0' && nb > j)
 	{
 		dest[i] = src[j];
 		i++;
--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,30 +6,42 @@
 /*   By: lealberg <lealberg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 15:32:56 by lealberg          #+#    #+#             */
-/*   Updated: 2022/04/18 14:41:53 by lealberg         ###   ########.fr       */
+/*   Updated: 2022/04/22 13:57:22 by lealberg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int				i;
-	unsigned int	src_size;
-	unsigned int	dest_size;
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	len_src;
+	unsigned int	len_dest;
 
-	src_size = 0;
-	while (src[src_size])
-		src_size++;
-	if (size == 0)
-		return (src_size);
-	dest_size = 0;
-	while (dest[dest_size])
-		dest_size++;
-	i = 0;
-	while (size && (--size - dest_size) && src[i])
+	j = 0;
+	i = ft_strlen(dest);
+	len_src = ft_strlen(src);
+	len_dest = ft_strlen(dest);
+	if (size < 1)
+		return (len_src + size);
+	while ((src[j] != '\0' && i < size - 1))
 	{
-		dest[dest_size + i] = src[i];
+		dest[i] = src[j];
 		i++;
+		j++;
 	}
-	dest[dest_size + i] = '\0';
-	return (dest_size + src_size);
+	dest[i] = '\0';
+	if (size < len_dest)
+		return (len_src + size);
+	else
+		return (len_dest + len_src);
 }
